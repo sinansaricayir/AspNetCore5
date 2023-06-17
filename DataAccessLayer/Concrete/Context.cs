07363b71715365f193337e4005d00f6b57f0c1eb
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-16 23:57:56 -0700

    Service List

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -12,7 +12,7 @@ namespace DataAccessLayer.Concrete
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-L7F1GSV\\SQLEXPRESS;database=CoreProje;integrated security=true");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-E22MHBD;database=CoreProje;integrated security=true");
         }
 
         public DbSet<About> Abouts { get; set; }
--- DataAccessLayer/Migrations/20230617043656_mig1.Designer.cs
+++ DataAccessLayer/Migrations/20230617043656_mig1.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    [Migration("20230612202616_mig1")]
+    [Migration("20230617043656_mig1")]
     partial class mig1
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
