6246e55695603fd62079b6c0af15582b6ef70702
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-17 23:58:21 +0300

    Skill List

---
+++ Core_Proje/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Skill
+{
+    public class SkillList : ViewComponent
+    {
+        SkillManager skillManager = new SkillManager();
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
