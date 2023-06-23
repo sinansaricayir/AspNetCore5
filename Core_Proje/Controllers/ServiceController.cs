0f3dd64eb2396cca5a6ca57c8a5e07991684aa32
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-24 00:41:21 +0300

    Service CRUD & New Admin Theme

---
+++ Core_Proje/Controllers/ServiceController.cs
@@ -0,0 +1,79 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class ServiceController : Controller
+    {
+
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hizmetler Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmetler Listesi";
+            var services = serviceManager.TGetList();
+            return View(services);
+        }
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+            ViewBag.v1 = "Hizmetler Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmetler Listesi";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            ViewBag.v1 = "Hizmetler Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmetler Listesi";
+
+            serviceManager.TAdd(service);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteService(int id)
+        {
+            var service = serviceManager.TGetById(id);
+            serviceManager.TDelete(service);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            ViewBag.v1 = "Hizmetler Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmetler Listesi";
+
+            var service = serviceManager.TGetById(id);
+
+            return View(service);
+        }
+
+
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            ViewBag.v1 = "Hizmetler Listesi";
+            ViewBag.v2 = "Hizmetler";
+            ViewBag.v3 = "Hizmetler Listesi";
+
+            serviceManager.TUpdate(service);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
