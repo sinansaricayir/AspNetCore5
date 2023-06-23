b427e93354f5be6cbe7a8acefbeef6d07ff4dd7f
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-23 20:28:41 +0300

    Admin | Feature & About CRUD

---
+++ Core_Proje/Controllers/AboutController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Hakkımızda Listesi";
+            ViewBag.v2 = "Hakkımızda";
+            ViewBag.v3 = "Hakkımızda Listesi";
+
+            var about = aboutManager.TGetById(1);
+
+            return View(about);
+        }
+
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            ViewBag.v1 = "Hakkımızda Listesi";
+            ViewBag.v2 = "Hakkımızda";
+            ViewBag.v3 = "Hakkımızda Listesi";
+
+            aboutManager.TUpdate(about);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
--- Core_Proje/Controllers/FeatureController.cs
+++ Core_Proje/Controllers/FeatureController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,30 @@ namespace Core_Proje.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            ViewBag.v1 = "Öne Çıkanlar Listesi";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkanlar Listesi";
+
+            var feature = featureManager.TGetById(1);
+
+            return View(feature);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            ViewBag.v1 = "Öne Çıkanlar Listesi";
+            ViewBag.v2 = "Öne Çıkanlar";
+            ViewBag.v3 = "Öne Çıkanlar Listesi";
+
+            featureManager.TUpdate(feature);
+
+            return RedirectToAction("Index", "Default");
         }
     }
 }
