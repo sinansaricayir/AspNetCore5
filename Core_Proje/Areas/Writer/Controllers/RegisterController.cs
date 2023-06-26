8773ec5ff5558c1cd5f1189c75b4a15c84d75da6
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-26 22:20:30 +0300

    AspNet Core Identity Operations

---
+++ Core_Proje/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Core_Proje.Areas.Writer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(UserRegisterViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adı Giriniz!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifre Giriniz!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Giriniz!")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyumlu Değil!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Mail Adresi Giriniz!")]
+        public string Mail { get; set; }
+    }
+}
