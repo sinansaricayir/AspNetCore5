7c07f20e22eed1712d4d0b06ce940a5978da695b
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-27 22:55:40 +0300

    Writer Panel | Announcement Page

---
+++ BusinessLayer/Concrete/AnnouncementManager.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AnnouncementManager : IAnnouncementService
+    {
+
+        IAnnouncementDal _announcement;
+
+        public AnnouncementManager(IAnnouncementDal announcement)
+        {
+            _announcement = announcement;
+        }
+
+        public void TAdd(Announcement t)
+        {
+            _announcement.Insert(t);
+        }
+
+        public void TDelete(Announcement t)
+        {
+            _announcement.Delete(t);
+        }
+
+        public Announcement TGetById(int id)
+        {
+            return _announcement.GetByID(id);
+        }
+
+        public List<Announcement> TGetList()
+        {
+            return _announcement.GetList();
+        }
+
+        public void TUpdate(Announcement t)
+        {
+            _announcement.Update(t);
+
+        }
+    }
+}
--- DataAccessLayer/Migrations/20230627191134_mig_addcolumn_announcement.Designer.cs
+++ DataAccessLayer/Migrations/20230627191134_mig_addcolumn_announcement.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230627191134_mig_addcolumn_announcement")]
+    partial class mig_addcolumn_announcement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
