eb08fc0a159dac9f68a3e5d161df3da78c6d00d2
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-28 19:17:28 +0300

    Update Profile | Upload UserImage

--- Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +13,45 @@ namespace Core_Proje.Areas.Writer.Controllers
     [Area("Writer")]
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var authUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = authUser.Name;
+            model.Surname = authUser.Surname;
+            model.PictureURL = authUser.ImageUrl;
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory();
+                var extension = Path.GetExtension(p.Picture.FileName);
+                var imageName = Guid.NewGuid() + extension;
+                var saveLocation = resource + "/wwwroot/userImage/" + imageName;
+                var stream = new FileStream(saveLocation, FileMode.Create);
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imageName;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
