920243baf75cd42c761b58657805d1b3e220d9a3
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-06-30 02:15:55 +0300

    Writer Panel | Notification & Navbar

---
+++ Core_Proje/Areas/Writer/ViewComponents/Account.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.ViewComponents
+{
+    public class Account : ViewComponent
+    {
+        private readonly UserManager<WriterUser> _userManager;
+
+        public Account(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var profile = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.profileImage = profile.ImageUrl;
+            return View();
+        }
+    }
+}
