28263efacb566e2a4ace78bf81a2204221b97698
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-07-07 21:04:35 +0300

    Admin Dashboard | ContactInfo CRUD

---
+++ Core_Proje/Controllers/ContactInfoController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class ContactInfoController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var contactInfo = contactManager.TGetList().FirstOrDefault();
+            return View(contactInfo);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contactManager.TUpdate(contact);
+            return RedirectToAction("Index");
+        }
+    }
+}
