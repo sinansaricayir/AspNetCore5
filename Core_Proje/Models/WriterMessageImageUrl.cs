ca1529556f6e1e3f383dbdb3d44566da5291c8d1
Sinan Sarıçayır <sinansaricayir@gmail.com>
2023-07-08 21:21:42 +0300

    Navbar Message List With Image

---
+++ Core_Proje/Models/WriterMessageImageUrl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Models
+{
+    public class WriterMessageImageUrl
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string SenderName { get; set; }
+        public string MessageContent { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Core_Proje/ViewComponents/Dashboard/NavbarMessageList.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using Core_Proje.Models;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class NavbarMessageList : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            using var context = new Context();
+            List<WriterMessageImageUrl> MessageData = new List<WriterMessageImageUrl>();
+            var email = "sinansaricayir@gmail.com";
+            var admin = context.Users.FirstOrDefault(x => x.Email == email);
+            var message = context.WriterMessages.Where(x => x.Reciever == admin.Email).ToList();
+
+            if (admin != null)
+            {
+                if (message != null)
+                {
+                    foreach (var item in message)
+                    {
+                        var img = context.Users.FirstOrDefault(x => x.Email == item.Sender);
+                        MessageData.Add(new WriterMessageImageUrl()
+                        {
+                            Id = item.WriterMessageID,
+                            Date = item.Date,
+                            SenderName = item.SenderName,
+                            MessageContent = item.MessageContent,
+                            ImageUrl = img.ImageUrl,
+                        });
+                    }
+                }
+            }
+
+            return View(MessageData);
+        }
+    }
+}
